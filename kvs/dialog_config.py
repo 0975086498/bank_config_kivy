7b4cb4c986b427a0b29da60fa538931cc83a0914
Nguyễn Trọng Hải <hai.nguyen.hp.k01@gmail.com>
2022-12-18 16:57:18 +0700

    update code

--- kvs/dialog_config.py
+++ kvs/dialog_config.py
@@ -5,7 +5,7 @@ from kivy.uix.scrollview import ScrollView
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.textfield import MDTextField
-
+from kivy.core.window import Window
 
 class DialogConfig(ScrollView):
     bank_config = DictProperty()
@@ -14,13 +14,12 @@ class DialogConfig(ScrollView):
     def __init__(self, bank_config):
         self.fields = bank_config.fields
         super().__init__()
+        window_sizes = Window.size
         self.bank_config = bank_config
-
+        self.height = (window_sizes[1] * 60)/100
         for field in self.bank_config.fields:
             textField = MDTextField()
             self.list_text_field.append(textField)
-
             textField.text = field['value']
             textField.hint_text = field['label']
             self.ids['contentLayout'].add_widget(textField)
-
